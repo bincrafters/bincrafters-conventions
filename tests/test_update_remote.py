bdc3182a2762c20e7c79fba48b6a10396b36ea83
Uilian Ries <uilianries@gmail.com>
2018-05-05 20:50:37 -0300

    Test user project update
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- tests/test_update_remote.py
+++ tests/test_update_remote.py
@@ -3,9 +3,16 @@
 
 from conan_clang_update.conan_clang_update import Command
 
-def test_update_clang_remote():
+def test_update_clang_remote_project():
     """ Clone dummy project and update it.
     """
     args = ['--remote', 'uilianries/conan-base64', '--skip-push']
+    command = Command()
+    command.run(args)
+
+def test_update_clang_remote_user():
+    """ Clone dummy project and update it.
+    """
+    args = ['--remote', 'uilianries', '--skip-push']
     command = Command()
     command.run(args)
