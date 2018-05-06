e4782f05069ac221d8208583aa55e07c2c7943f7
Uilian Ries <uilianries@gmail.com>
2018-05-05 21:21:39 -0300

    Test project pattern
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- tests/test_update_remote.py
+++ tests/test_update_remote.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from conan_clang_update.conan_clang_update import Command
+import platform
+
+
+def is_macos():
+    return 'Darwin' in platform.system()
 
 
 def test_update_clang_remote_project():
@@ -13,8 +18,24 @@ def test_update_clang_remote_project():
 
 
 def test_update_clang_remote_user():
-    """ Clone dummy project and update it.
+    """ Clone all projects and update it.
     """
+    # XXX (uilianries): There is some error to request Github API on Mac jobs
+    if is_macos():
+        return
+
     args = ['--remote', 'uilianries', '--skip-push']
+    command = Command()
+    command.run(args)
+
+
+def test_update_clang_remote_user_project_pattern():
+    """ Clone only filtered projects and update it.
+    """
+    # XXX (uilianries): There is some error to request Github API on Mac jobs
+    if is_macos():
+        return
+
+    args = ['--remote', 'uilianries', '--skip-push', '--projects-pattern', 'uilianries/conan-*']
     command = Command()
     command.run(args)
