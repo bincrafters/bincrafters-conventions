61800ab87efc2b3662bd54e9644158d410e7575b
Croydon <cr0ydon@outlook.com>
2019-01-02 06:31:12 +0100

    Fix pyenv python version updates

--- bincrafters_conventions/actions/update_other_pyenv_python_version.py
+++ bincrafters_conventions/actions/update_other_pyenv_python_version.py
@@ -2,12 +2,12 @@ def update_other_pyenv_python_version(main, file, current_python_version, check_
     """ Replaces which Python version is getting installed in .ci/install.sh for macOS via pyenv
     """
 
-    current_install_string = "pyenv install {}".format(current_python_version)
-    current_virtualenv_string = "pyenv virtualenv {} conan".format(current_python_version)
+    current_install_string = "pyenv install {}\n".format(current_python_version)
+    current_virtualenv_string = "pyenv virtualenv {} conan\n".format(current_python_version)
 
     for old_version in check_for_old_versions:
-        old_install_string = "pyenv install {}".format(old_version)
-        old_virtualenv_string = "pyenv virtualenv {} conan".format(old_version)
+        old_install_string = "pyenv install {}\n".format(old_version)
+        old_virtualenv_string = "pyenv virtualenv {} conan\n".format(old_version)
 
         if main.file_contains(file, old_install_string):
             if (main.replace_in_file(file, old_install_string, current_install_string) and
