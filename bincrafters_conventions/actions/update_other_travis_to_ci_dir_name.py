753b9c1b77a89a2d4d3f6f0f17e3d71287ea8f43
Croydon <cr0ydon@outlook.com>
2019-01-08 10:07:23 +0100

    Fix renaming of .travis/ to .ci/ on Windows

--- bincrafters_conventions/actions/update_other_travis_to_ci_dir_name.py
+++ bincrafters_conventions/actions/update_other_travis_to_ci_dir_name.py
@@ -5,8 +5,8 @@ import os
 def update_other_travis_to_ci_dir_name(main):
     """ Replace Travis directory by CI dir
     """
-    travis_dir = ".travis"
-    ci_dir = ".ci"
+    travis_dir = ".travis/"
+    ci_dir = ".ci/"
     if os.path.isdir(travis_dir):
         shutil.move(os.path.abspath(travis_dir), os.path.abspath(ci_dir))
         main.output_result_update("Update Travis directory path from .travis -> .ci")
