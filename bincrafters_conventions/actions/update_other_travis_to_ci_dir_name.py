6ecdc7aab4e776c29a1469f166063f8abccd3011
Croydon <cr0ydon@outlook.com>
2019-03-31 01:20:19 +0100

    Update travis -> .ci: rather use os.rename

--- bincrafters_conventions/actions/update_other_travis_to_ci_dir_name.py
+++ bincrafters_conventions/actions/update_other_travis_to_ci_dir_name.py
@@ -1,4 +1,4 @@
-import shutil
+# import shutil
 import os
 
 
@@ -8,7 +8,8 @@ def update_other_travis_to_ci_dir_name(main):
     travis_dir = ".travis/"
     ci_dir = ".ci/"
     if os.path.isdir(travis_dir):
-        shutil.move(os.path.abspath(travis_dir), os.path.abspath(ci_dir))
+        # shutil.move(os.path.abspath(travis_dir), os.path.abspath(ci_dir))
+        os.rename(os.path.abspath(travis_dir), os.path.abspath(ci_dir))
         main.output_result_update("Update Travis directory path from .travis -> .ci")
         return True
     return False
