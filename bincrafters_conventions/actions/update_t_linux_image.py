3efaf7283641f08b7ec6fe6c87860feeb55c65e2
Uilian Ries <uilianries@gmail.com>
2019-04-03 18:54:16 +0200

    Update Travis Linux image: Check first if new key exists
    Co-Authored-By: Croydon <cr0ydon@outlook.com>

--- bincrafters_conventions/actions/update_t_linux_image.py
+++ bincrafters_conventions/actions/update_t_linux_image.py
@@ -7,7 +7,8 @@ def update_t_linux_image(main, file):
     }
 
     for old_image_version, new_image_version in image_update_mapping.items():
-        if main.file_contains(file, old_image_version):
+        if main.file_contains(file, old_image_version) and \
+           not main.file_contains(file, new_image_version):
             if (main.replace_in_file(file, old_image_version, new_image_version)):
                 main.output_result_update(title="Travis: Update Linux CI image from {} to {}".format(old_image_version, new_image_version))
                 return True
