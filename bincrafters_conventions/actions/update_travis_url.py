21be5dc30aa8dc3538ebfc9a4b19c6a3c014acee
Michael "Croydon" Keck <cr0ydon@outlook.com>
2019-04-12 16:56:03 -0300

    Add README message detail
    Co-Authored-By: uilianries <uilianries@gmail.com>

--- bincrafters_conventions/actions/update_travis_url.py
+++ bincrafters_conventions/actions/update_travis_url.py
@@ -2,6 +2,6 @@ def update_travis_url(main, file):
     """ Update Travis CI URL
     """
     if main.replace_in_file(file, "https://travis-ci.org/bincrafters", "https://travis-ci.com/bincrafters"):
-        main.output_result_update(title="README: Update Travis URL")
+        main.output_result_update(title="README: Update Travis URL from .org to .com")
         return True
     return False
