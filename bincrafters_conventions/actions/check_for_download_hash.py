98585771aa35b313b57f5a26db193a376b6947e6
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-07-05 18:04:13 +0200

    Use ast for checking download hash

--- bincrafters_conventions/actions/check_for_download_hash.py
+++ bincrafters_conventions/actions/check_for_download_hash.py
@@ -1,21 +1,44 @@
-import re
+# -*- coding: utf-8 -*-
+
+import ast
+
+
+class CheckToolsGetVisitor(ast.NodeVisitor):
+    def __init__(self):
+        super().__init__()
+        self.matches = []
+
+    def visit_Call(self, node):
+        def check_node(node):
+            if isinstance(node.func, ast.Attribute) and isinstance(node.func.value, ast.Name):
+                if node.func.value.id != "tools" or node.func.attr != "get":
+                    return
+                sha256 = False
+                for kw in node.keywords:
+                    if kw.arg == 'sha256':
+                        sha256 = True
+                self.matches.append({"sha256": sha256, "lineno": node.lineno, "col_offset": node.col_offset})
+        check_node(node)
+        self.generic_visit(node)
 
 
 def check_for_download_hash(main, file):
-    conanfile = open(file, 'r')
+    conanfile = open(file, "r")
     recipe = conanfile.read()
     conanfile.close()
 
-    result_passed = None
+    v = CheckToolsGetVisitor()
+    n = ast.parse(recipe)
+    v.visit(n)
 
-    for m in re.finditer(r"tools\.get\((?P<args>.*?[^\\])\)", recipe, re.DOTALL):
-        if re.search(r"sha256\s*=\s*(['\"]).*?\1", m["args"], re.DOTALL):
-            main.output_result_check(passed=True, title="SHA256 hash in tools.get()")
-            result_passed = True if result_passed is None else result_passed
-        else:
-            main.output_result_check(passed=False, title="SHA256 hash in tools.get()", reason="checksum not found")
-            result_passed = False
-    if result_passed is None:
+    if not v.matches:
         main.output_result_check(passed=True, skipped=True, title="SHA256 hash in tools.get()", reason="tools.get() isn't used")
-        result_passed = True
-    return result_passed
+        return True
+
+    for match in v.matches:
+        if match["sha256"]:
+            main.output_result_check(passed=True, title="SHA256 hash in tools.get()")
+        else:
+            main.output_result_check(passed=False, title="SHA256 hash in tools.get()",
+                                     reason="checksum not found ({file}:{line})".format(file=file, line=match["lineno"]))
+    return all(m["sha256"] for m in v.matches)
