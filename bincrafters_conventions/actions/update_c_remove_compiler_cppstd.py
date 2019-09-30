752c32060ddc043d8fd05dbe754287841057ae96
Croydon <git@cr0ydon.com>
2019-09-30 17:05:57 +0200

    Fix: Deletion of compiler.cppstd should be only added once

--- bincrafters_conventions/actions/update_c_remove_compiler_cppstd.py
+++ bincrafters_conventions/actions/update_c_remove_compiler_cppstd.py
@@ -1,13 +1,15 @@
 def update_c_remove_compiler_cppstd(main, file):
     # Remove self.settings.compiler.cppstd as well, when self.settings.compiler.libcxx is getting removed
     search_string = "del self.settings.compiler.libcxx"
-    with open(file) as ifd:
-        for line in ifd:
-            if line.strip() == search_string:
-                new_string = line.replace(search_string, "del self.settings.compiler.cppstd")
-                new_line = line + new_string
+    new_string = "del self.settings.compiler.cppstd"
+    if not main.file_contains(file, new_string):
+        with open(file) as ifd:
+            for line in ifd:
+                if line.strip() == search_string:
+                    new_string = line.replace(search_string, new_string)
+                    new_line = line + new_string
 
-                if main.replace_in_file(file, line, new_line):
-                    main.output_result_update(title="Add del self.settings.compiler.cppstd")
-                    return True
+                    if main.replace_in_file(file, line, new_line):
+                        main.output_result_update(title="Add del self.settings.compiler.cppstd")
+                        return True
     return False
