b63930a54f031a80a8c853f2fc9c529b6cf6a06c
Croydon <git@cr0ydon.com>
2020-02-11 12:39:35 +0100

    Release 0.18.3

--- bincrafters_conventions/bincrafters_conventions.py
+++ bincrafters_conventions/bincrafters_conventions.py
@@ -43,7 +43,7 @@ from .actions.update_other_pyenv_python_version import update_other_pyenv_python
 from .actions.update_readme_travis_url import update_readme_travis_url
 
 
-__version__ = '0.18.2'
+__version__ = '0.18.3'
 __author__ = 'Bincrafters <bincrafters@gmail.com>'
 __license__ = 'MIT'
 
