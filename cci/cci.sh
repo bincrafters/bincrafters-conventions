9df444d434ea969793295c7e27f356e18e9df18b
Croydon <git@cr0ydon.com>
2020-09-09 17:26:30 +0200

    Integrate cron job into master branch

---
+++ cci/cci.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -e
+
+git remote rm origin
+git remote add origin https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${APPVEYOR_REPO_NAME}.git
+
+git fetch origin master
+git fetch origin ${APPVEYOR_REPO_BRANCH}
+git checkout ${APPVEYOR_REPO_BRANCH}
+git push --set-upstream origin ${APPVEYOR_REPO_BRANCH}
+
+# Is this build triggered by a convention update?
+if [[ "$(git log -1 --pretty=%an)" == bincrafters* ]]; then
+  echo "This CI run was triggered by a convention update, we don't need to run bincrafters-conventions again";
+  exit 0 ;
+fi
+
+dirpath="none"
+for directory in $(git diff --dirstat=files,0 `git merge-base origin/master ${APPVEYOR_REPO_BRANCH}`..${APPVEYOR_REPO_BRANCH} | sed 's/^[ 0-9.]\+% //g'); do
+  if [[ "${directory}" == recipes/* ]]; then
+    count=$(echo "${directory}" | awk -F"/" '{print NF-1}');
+    if [[ "${count}" -ge "3" ]]; then
+      dirpath="${directory}"
+    fi;
+  fi;
+done
+
+
+if [[ "${dirpath}" == "none" ]]; then
+  echo "This branch didn't change any recipe. Exiting.";
+  exit 0 ;
+fi
+
+recipename=${dirpath#*recipes/}
+recipename=${recipename%%/*}
+recipepath="recipes/${recipename}/"
+version=${dirpath#"$recipepath"}
+version=${version%%/*}
+recipepath="recipes/${recipename}/${version}"
+
+echo ${recipename}
+
+echo ${recipepath}
+cd ${recipepath}
+
+source $HOME/venv3.7/bin/activate
+
+python --version
+pip --version
+
+git config --global user.email "bincrafters@gmail.com"
+git config --global user.name "bincrafters-user"
+
+pip install --quiet bincrafters-conventions
+conan user  # initialize Conan registry file
+
+bincrafters-conventions
+
+
+git add -u 
+git reset -- cci.sh
+
+git diff-index --quiet HEAD || git commit -a -m "${recipename}: Update Conan conventions" -m "Automatically created by $(bincrafters-conventions --version)" && git push
---
+++ cci/update_from_upstream.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Installs & configurations
+git config --global user.email "bincrafters@gmail.com"
+git config --global user.name "bincrafters-user"
+
+
+# Update Bincrafters' fork of Conan Center Index
+git clone "https://${GIT_GITHUB_USERNAME}:${GIT_GITHUB_TOKEN}@github.com/bincrafters/conan-center-index"
+cd conan-center-index
+git remote add upstream https://github.com/conan-io/conan-center-index
+git fetch upstream
+git reset --hard upstream/master
+git push -f
+
+
+# Delete all merged branches in our fork 
+git branch -r --merged master | grep -v master | sed 's/origin\///' | xargs -r -n 1 git push --delete origin
