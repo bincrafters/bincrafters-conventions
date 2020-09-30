26e7319bffd2a4f3075d82db7d9a7f20379bb752
Croydon <git@cr0ydon.com>
2020-09-30 11:18:35 +0200

    Update update_from_upstream.sh

--- cci/update_from_upstream.sh
+++ cci/update_from_upstream.sh
@@ -6,21 +6,58 @@ set -e
 git config --global user.email "bincrafters@gmail.com"
 git config --global user.name "bincrafters-user"
 
-# Debug
-echo all
-git config --list
-echo global
-git config --global --list
-# Update our fork of Conan Center Index
+
+# Install GitHub CLI tool
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
+
+echo ${GIT_GITHUB_TOKEN} | gh auth login --with-token
+
+
+# Clone the CCI fork
 git clone "https://${GIT_GITHUB_USERNAME}:${GIT_GITHUB_TOKEN}@github.com/${GIT_GITHUB_FORK_ACCOUNT}/conan-center-index"
 cd conan-center-index
+
+# Get current master commit
+# TODO
+
+###
+### Update CCI fork
+###
 git remote add upstream https://github.com/conan-io/conan-center-index
 git fetch upstream
 git reset --hard upstream/master
 git push -f
 
 
+###
+### Delete all merged branches in our fork which got merged via a merge commit
+###
+
+# TOOD: Debug
 git branch -r --merged master
 
-# Delete all merged branches in our fork 
+# Get all commit messages between old master commit and newest one 
+# TODO
+
+# Isolate the ID of merged PRs from the commit messages
+# PR_IDS have to be space separated
+# TODO
+PR_IDS="2984"
+
+for PR_ID in ${PR_IDS}
+do
+    # Check if this is a PR from $GIT_GITHUB_FORK_ACCOUNT and also if it is actually meged
+    # $'\t' stands for a tab character
+    PR_INFORMATION=$(gh pr list --limit 200 --state merged | grep $'\t'"${GIT_GITHUB_FORK_ACCOUNT}:" | grep "${PR_ID}"$'\t')
+    echo ${PR_INFORMATION}
+    # Retrieve the branch name and delete it
+    # TODO
+done
+
+###
+### Delete all merged branches in our fork which got NOT merged via a merge commit
+###
 git branch -r --merged master | grep -v master | sed 's/origin\///' | xargs -r -n 1 git push --delete origin
