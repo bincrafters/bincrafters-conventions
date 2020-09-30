37e62b7700d21f5e65dbaa326aed0356f2ffd8db
Croydon <git@cr0ydon.com>
2020-09-30 14:22:18 +0200

    Update update_from_upstream.sh

--- cci/update_from_upstream.sh
+++ cci/update_from_upstream.sh
@@ -88,9 +88,9 @@ do
     echo "${PR_ID}"
     # Check if this is a PR from $GIT_GITHUB_FORK_ACCOUNT and also if it is actually meged
     # $'\t' stands for a tab character
-    echo "${RECENT_PRS}" | grep "${PR_ID}"$'\t'
-    # PR_INFORMATION=$(echo "${RECENT_PRS}" | grep "${PR_ID}"$'\t')
-    # echo "${PR_INFORMATION}"
+    # || true because the CI should not "fail" when the last PR is not a PR from $GIT_GITHUB_FORK_ACCOUNT
+    PR_INFORMATION=$(echo "${RECENT_PRS}" | grep "^${PR_ID}"$'\t') || true
+    echo "${PR_INFORMATION}"
 
     # Retrieve the branch name and delete it
     # TODO
@@ -98,8 +98,8 @@ do
     # -n to not get any output if there is (no) match
     # -r to enable extend regex syntax
     # /p to print matches despite -n
-    # BRANCH_NAME=$(echo "${PR_INFORMATION}" | sed -nr 's/([0-9]*)\t(.*)\t(.*)'"${GIT_GITHUB_FORK_ACCOUNT}:"'(.*)\t(.*)/\4/p')
-    # echo "${PR_INFORMATION}" | sed -nr 's/([0-9]*)\t(.*)\t(.*)'"${GIT_GITHUB_FORK_ACCOUNT}:"'(.*)\t(.*)/\4/p' | xargs -r -n 1 echo
+    BRANCH_NAME=$(echo "${PR_INFORMATION}" | sed -nr 's/([0-9]*)\t(.*)\t(.*)'"${GIT_GITHUB_FORK_ACCOUNT}:"'(.*)\t(.*)/\4/p')
+    echo "${PR_INFORMATION}" | sed -nr 's/([0-9]*)\t(.*)\t(.*)'"${GIT_GITHUB_FORK_ACCOUNT}:"'(.*)\t(.*)/\4/p' | xargs -r -n 1 echo
 done
 
 ###
