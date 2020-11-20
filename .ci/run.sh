307847bf97830efe756f4ea1efa9001805fa6f03
Croydon <git@cr0ydon.com>
2020-11-20 19:56:14 +0100

    CI scripts: remove dead code

--- .ci/run.sh
+++ .ci/run.sh
@@ -2,13 +2,6 @@
 
 set -ex
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
-    fi
-    pyenv activate conan
-fi
-
 python setup.py sdist
 pushd tests
 pytest -v -s --cov=bincrafters_conventions
