6dfef3bde5906f0cfb50aa81cf8d22b7e8690349
Croydon <git@cr0ydon.com>
2021-02-06 15:21:15 +0100

    Update GHA workflow to version 10

--- bincrafters_conventions/actions/update_gha.py
+++ bincrafters_conventions/actions/update_gha.py
@@ -11,28 +11,31 @@ def update_gha(main, file):
     file_content = ""
     tag = "# bincrafters-conventions:gha-workflow-version:"
 
-    with open(os.path.join(os.path.dirname(__file__), "update_gha.yml")) as ifd:
+    with open(os.path.join(os.path.dirname(__file__), "update_gha.yml"), encoding="utf-8") as ifd:
         for line in ifd:
             if line.strip().startswith(tag):
                 gha_workflow_version = line.strip().replace(tag, "")
                 break
 
-    with open(file) as ifd:
+    with open(file, encoding="utf-8") as ifd:
         for line in ifd:
             if line.strip().startswith(tag):
                 workflow_version = line.strip().replace(tag, "")
-                if workflow_version >= gha_workflow_version:
+                if int(workflow_version) >= int(gha_workflow_version):
                     return False
                 else:
                     break
             else:
                 file_content += line
 
+    if int(workflow_version) < 10:
+        file_content += "on: [push, pull_request]\n\n"
+
     basepath = os.path.dirname(__file__)
-    with open(os.path.join(basepath, "update_gha.yml")) as fp:
+    with open(os.path.join(basepath, "update_gha.yml"), encoding="utf-8") as fp:
         file_content += fp.read()
 
-    with open(file, 'w') as fd:
+    with open(file, 'w', encoding="utf-8") as fd:
         fd.write(file_content)
 
     update_message = "GitHub Actions: Update CI script to version {}".format(gha_workflow_version)
