740645518a2bee12041618f979610b605be63238
Croydon <git@cr0ydon.com>
2022-03-15 06:12:11 +0100

    Remove Readme update for Travis URL updates

--- bincrafters_conventions/actions/update_readme_travis_url.py
+++
@@ -1,7 +0,0 @@
-def update_readme_travis_url(main, file):
-    """ Update Travis CI URL
-    """
-    if main.replace_in_file(file, "https://travis-ci.org/bincrafters", "https://travis-ci.com/bincrafters"):
-        main.output_result_update(title="README: Update Travis URL from .org to .com")
-        return True
-    return False
