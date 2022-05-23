a400aae829f7b32dfc0f21c22665c72af6185a98
ericLemanissier <ericLemanissier@users.noreply.github.com>
2022-05-23 19:01:01 +0200

    fix openssl 3.0 reference update

--- bincrafters_conventions/bincrafters_conventions.py
+++ bincrafters_conventions/bincrafters_conventions.py
@@ -61,7 +61,7 @@ openssl_version_matrix = {'1.0.1': {'latest_patch': 'h', 'eol': True},
                           '1.0.2': {'latest_patch': 'u', 'eol': True},
                           '1.1.0': {'latest_patch': 'l', 'eol': True},
                           '1.1.1': {'latest_patch': 'm', 'eol': False},
-                          '3.0': {'latest_patch': '1', 'eol': False},
+                          '3.0.': {'latest_patch': '1', 'eol': False},
                           }
 
 @contextlib.contextmanager
