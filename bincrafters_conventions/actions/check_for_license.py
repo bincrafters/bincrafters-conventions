4bf67f524de14998e517663cec162e47b76da84a
Croydon <git@cr0ydon.com>
2022-10-25 21:06:07 +0200

    Fix check for LICENSE file for CCI-style repos
    It is enough if there is a LICENSE file in the top level directory

--- bincrafters_conventions/actions/check_for_license.py
+++ bincrafters_conventions/actions/check_for_license.py
@@ -3,9 +3,15 @@ import os
 
 def check_for_license(main):
     if (os.path.isfile("license") or os.path.isfile("LICENSE") or
-            os.path.isfile("license.md") or os.path.isfile("LICENSE.md")):
-        main.output_result_check(passed=True, title="Recipe license")
+            os.path.isfile("license.md") or os.path.isfile("LICENSE.md")) or \
+            (os.path.isdir(os.path.join("..", "..", "..", "recipes")) and (
+                    os.path.isfile(os.path.join("..", "..", "..", "license")) or
+                    os.path.isfile(os.path.join("..", "..", "..", "LICENSE")) or
+                    os.path.isfile(os.path.join("..", "..", "..", "license.md")) or
+                    os.path.isfile(os.path.join("..", "..", "..", "LICENSE.md"))
+            )):
+        main.output_result_check(passed=True, title="Recipe license file")
         return True
-    main.output_result_check(passed=False, title="Recipe license",
+    main.output_result_check(passed=False, title="Recipe license file",
                              reason="could not be found. Please add a LICENSE.md for the recipe")
     return False
