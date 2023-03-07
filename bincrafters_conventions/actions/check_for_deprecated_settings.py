71109afe8ed5992daf68de2b7880c948b66d456e
Croydon <git@cr0ydon.com>
2023-03-07 20:16:54 +0100

    Fix check_for_deprecated_settings

--- bincrafters_conventions/actions/check_for_deprecated_settings.py
+++ bincrafters_conventions/actions/check_for_deprecated_settings.py
@@ -1,7 +1,7 @@
 def check_for_deprecated_settings(main, file):
     """ Check if the file has all valid settings
     """
-    settings = main._compat_api.graph.compat_inspect_attribute(conanfile=file, attribute="generators")
+    settings = main._compat_api.graph.compat_inspect_attribute(conanfile=file, attribute="settings")
 
     if settings and "cppstd" in settings:
         main.output_result_check(passed=False, title="Deprecated settings",
