7bf9f3269b1b71d1d533259ff0b20ccb91f8e9b7
Croydon <git@cr0ydon.com>
2023-03-07 21:21:19 +0100

    Fix check_for_download_hash

--- bincrafters_conventions/actions/check_for_download_hash.py
+++ bincrafters_conventions/actions/check_for_download_hash.py
@@ -1,17 +1,19 @@
 import os
 
-
+# FIXME: This is too unflexible for the recent varities of source() mutations
 def check_for_download_hash(main, file):
 
-    if not os.path.exists("conandata.yml") and not main.file_contains(file, "tools.get"):
-        main.output_result_check(passed=True, skipped=True, title="Conandata.yml + tools.get()", reason="tools.get() isn't used")
+    if not os.path.exists("conandata.yml") and not main.file_contains(file, "tools.get") and not main.file_contains(file, "get(self"):
+        main.output_result_check(passed=True, skipped=True, title="Conandata.yml + tools.get()", reason="get() isn't used")
         return True
 
     if os.path.exists("conandata.yml")\
-            and main.file_contains(file, 'tools.get(**self.conan_data["sources"][self.version]'):
-        main.output_result_check(passed=True, title="Conandata.yml + tools.get()")
+        and main.file_contains("conandata.yml", "sha256")\
+            and (main.file_contains(file, 'tools.get(**self.conan_data["sources"][self.version]')\
+                or main.file_contains(file, 'get(self, **self.conan_data["sources"][self.version]')):
+        main.output_result_check(passed=True, title="Conandata.yml + get() is used")
         return True
 
     main.output_result_check(passed=False, title="Conandata.yml + tools.get()",
-                             reason="Either Conandata.yml doesn't exist or not used in tools.get()")
+                             reason="Either Conandata.yml doesn't exist or not used in get()")
     return False
