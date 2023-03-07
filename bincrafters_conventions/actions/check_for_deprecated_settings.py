459441b914ea640679fbac1e5cbf8b69ba533514
Croydon <git@cr0ydon.com>
2023-03-07 19:20:39 +0100

    Convert more checks to CompatAPI

--- bincrafters_conventions/actions/check_for_deprecated_settings.py
+++ bincrafters_conventions/actions/check_for_deprecated_settings.py
@@ -1,18 +1,7 @@
-from conans.client import conan_api
-from conan.errors import ConanException
-
-
 def check_for_deprecated_settings(main, file):
     """ Check if the file has all valid settings
     """
-    conan_instance, _, _ = conan_api.Conan.factory()
-    settings = None
-    field = "settings"
-
-    try:
-        settings = conan_instance.inspect(path=file, attributes=[field])[field]
-    except ConanException:
-        pass
+    settings = main._compat_api.graph.compat_inspect_attribute(conanfile=file, attribute="generators")
 
     if settings and "cppstd" in settings:
         main.output_result_check(passed=False, title="Deprecated settings",
