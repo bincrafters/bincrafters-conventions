8f481a2296f3d2407989efbc539676955fc523ca
Croydon <git@cr0ydon.com>
2023-03-08 15:33:02 +0100

    Update deprecated attributes: Check v1 test_packages; split v1/v2 logic

--- bincrafters_conventions/actions/update_c_deprecated_attributes.py
+++ bincrafters_conventions/actions/update_c_deprecated_attributes.py
@@ -8,44 +8,59 @@ def update_c_deprecated_attributes(main, file):
     :param file: Conan file path
     """
 
-    test_package = file.replace("conanfile.py", "test_package/conanfile.py")
+    test_v1_package = file.replace("conanfile.py", os.path.join("test_v1_package", "conanfile.py"))
+    test_package = file.replace("conanfile.py", os.path.join("test_package", "conanfile.py"))
 
     updated = False
 
-    for file in [file, test_package]:
-        conanfile = open(file, 'r')
+    # We need to go regularly though the changelog to catch new deprecations
+    # Last checked for Conan versions up to 1.16.1
+    deprecations_conan_v1 = {  # Official Conan attributes
+        # Conan 1.13.0
+        "self.cpp_info.cppflags": "self.cpp_info.cxxflags",
+
+        # Custom attributes
+        " install_subfolder =": " _install_subfolder =",
+        "self.install_subfolder": "self._install_subfolder",
+
+        " build_subfolder =": " _build_subfolder =",
+        "self.build_subfolder": "self._build_subfolder",
+
+        " source_subfolder =": " _source_subfolder =",
+        "self.source_subfolder": "self._source_subfolder",
+
+        "def configure_cmake": "def _configure_cmake",
+        "self.configure_cmake": "self._configure_cmake",
+
+        # Unknown
+        "self.requires.add": "self.requires",
+        "self.build_requires.add": "self.build_requires",
+
+        "tools.cross_building(self.settings)": "tools.cross_building(self)",
+    }
+
+    deprecations_conan_v2 = {
+    }
+
+    deprecations_v1_and_v2 = deprecations_conan_v1.copy()
+    deprecations_v1_and_v2 = deprecations_v1_and_v2.update(deprecations_conan_v2)
+
+    for cfile in [file, test_v1_package, test_package]:
+        if file == test_v1_package or file == test_package:
+            if not os.path.exists(file):
+                continue
+
+        conanfile = open(cfile, 'r')
         recipe = conanfile.read()
         conanfile.close()
 
-        # We need to go regularly though the changelog to catch new deprecations
-        # Last checked for Conan versions up to 1.14.1
-        deprecations = {  # Official Conan attributes
-            # Conan 1.13.0
-            "self.cpp_info.cppflags": "self.cpp_info.cxxflags",
-
-            # Custom attributes
-            " install_subfolder =": " _install_subfolder =",
-            "self.install_subfolder": "self._install_subfolder",
-
-            " build_subfolder =": " _build_subfolder =",
-            "self.build_subfolder": "self._build_subfolder",
-
-            " source_subfolder =": " _source_subfolder =",
-            "self.source_subfolder": "self._source_subfolder",
-
-            "def configure_cmake": "def _configure_cmake",
-            "self.configure_cmake": "self._configure_cmake",
-
-            # Unknown
-            "self.requires.add": "self.requires",
-            "self.build_requires.add": "self.build_requires",
-
-            "tools.cross_building(self.settings)": "tools.cross_building(self)",
-        }
+        deprecations = deprecations_conan_v1
+        if cfile == test_v1_package:
+            deprecations = deprecations_v1_and_v2
 
         for deprecated, replacement in deprecations.items():
             if deprecated in recipe:
-                if main.replace_in_file(file, deprecated, replacement):
+                if main.replace_in_file(cfile, deprecated, replacement):
                     main.output_result_update(title=f"Replace deprecated {deprecated} with {replacement}")
                 updated = True
 
