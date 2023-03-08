b1fd0c56238d9ccf5795483ac1f513f7227005fb
Croydon <git@cr0ydon.com>
2023-03-08 14:51:21 +0100

    Add more deprecated generators to check

--- bincrafters_conventions/actions/check_for_deprecated_generators.py
+++ bincrafters_conventions/actions/check_for_deprecated_generators.py
@@ -6,7 +6,7 @@ def check_for_deprecated_generators(main, file):
     generators = main._compat_api.graph.compat_inspect_attribute(conanfile=file, attribute="generators")
     generators = list(generators)
     for generator in generators:
-        if generator in ["gcc", "boost-build"]:
+        if generator in ["gcc", "boost-build", "cmake", "cmake_paths", "cmake_find_package", "cmake_find_package_multi", "visual_studio_legacy"]:
             main.output_result_check(passed=False, title="Deprecated generator",
                                      reason=f"generator: {generator}")
             return False
